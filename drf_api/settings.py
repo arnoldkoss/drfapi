1c87f89b158252fe242195c6ac229b942c303926
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-01 16:12:35 +0000

    set debug to false

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,7 +65,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
- DEBUG = 'DEV' in os.environ
+ DEBUG = False
 
 ALLOWED_HOSTS = ['8000-arnoldkoss-drfapi-yozrdld917j.ws-eu110.gitpod.io', 'drf-appi-ecb8a38b4305.herokuapp.com']
 
